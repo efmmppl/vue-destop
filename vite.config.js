57e1b04139f632fa04730daa56fa77c8a2707be9
yangmaolin <yangmaolin_lte@163.com>
2024-03-20 20:43:00 +0800

    test05

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   publicPath: './src',
-  base: './src',
+  base: '/vue-destop/',
   plugins: [
     vue(),
   ],
